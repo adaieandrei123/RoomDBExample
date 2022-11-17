62d58e68c704330b4c8913723165d0412a3f947f
ADAIE <gadau.andrei@gmail.com>
2022-11-17 03:58:09 +0200

    new update

--- app/src/main/java/com/example/roomdbexample/MainActivity.kt
+++ app/src/main/java/com/example/roomdbexample/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.roomdbexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.example.roomdbexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var recycler: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        recycler = binding.myRecyclerView
+
+        initRecyler()
+
+    }
+
+    private fun initRecyler() {
+        recycler.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(this)
+        recycler.layoutManager = layoutManager
     }
 }
---
+++ app/src/main/java/com/example/roomdbexample/model/NoteEntity.kt
@@ -0,0 +1,10 @@
+package com.example.roomdbexample.model
+
+import java.util.*
+
+
+data class NoteEntity(
+    var id: Int?,
+    var date: Date?,
+    var string: String?
+)
